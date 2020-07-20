67ceab34d338b507aaef82ba2de714e862c7c32b
unknown <kim70273@naver.com>
2020-07-21 02:54:14 +0900

    Updates

---
+++ precache-manifest.de97bb2677049e500d84ef6a95fa88d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f7134d33409a4d9a8f9f63bf963bb5",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "6af07e5d3549e9e7d158",
+    "url": "/movie_app_2020/static/css/main.54974f27.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af07e5d3549e9e7d158",
+    "url": "/movie_app_2020/static/js/main.28dc0c7a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
